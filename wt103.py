2d7c8e524e800207fd4dc3a76e12801339198df4
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2019-02-22 20:37:23 -0500

    Change to WT2

--- wt103.py
+++ wt103.py
@@ -5,7 +5,7 @@ import fastprogress
 
 fastprogress.fastprogress.SAVE_PATH = 'log.txt'
 
-path = Config().data_path()/'wikitext-103'
+path = Config().data_path()/'wikitext-2'
 
 data = load_data(path, bs=32, bptt=150)
 config = tfmerXL_lm_config.copy()
@@ -17,7 +17,7 @@ config['resid_p'] = 0.1
 save = partial(SaveModelCallback, every='epoch', name='tfmer12')
 learn = language_model_learner(data, TransformerXL, config=config, pretrained=False, clip=None, alpha=0, beta=2, callback_fns=save)
 learn = learn.to_fp16(clip=0.1, dynamic=True)
-learn.fit_one_cycle(10, 5e-4, div_factor=5)
+learn.fit(1, 5e-4)
 
 learn.save('tfmer12_wt103')
 pickle.dump(data.vocab.itos, open(path/'itos_tfm12.pkl', 'wb'))
