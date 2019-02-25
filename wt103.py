fecd02d7e54a2db68c246b001a258e80e8514a43
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2019-02-25 14:47:54 -0500

    Update

--- wt103.py
+++ wt103.py
@@ -1,12 +1,12 @@
-from fastai.text import * 
+from fastai.text import *
 from fastai.callbacks import SaveModelCallback
-from memory_profiler import profile
+#from memory_profiler import profile
 import fastprogress
 
 fastprogress.fastprogress.SAVE_PATH = 'log.txt'
 
 path = Config().data_path()/'wikitext-2'
-
+#path = "/raid/dldata/fastai/wikitext-2"
 data = load_data(path, bs=32, bptt=150)
 config = tfmerXL_lm_config.copy()
 config['output_p'] = 0.1
@@ -17,4 +17,3 @@ config['resid_p'] = 0.1
 learn = language_model_learner(data, TransformerXL, config=config, pretrained=False, clip=None, alpha=0, beta=2)
 learn = learn.to_fp16(clip=0.1, dynamic=True)
 learn.fit(1, 5e-4)
-
