fb368d45b0a5894bc4eeef09b96cf118482222bf
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-02-25 19:51:38 +0000

    Add data processing

--- wt103.py
+++ wt103.py
@@ -7,7 +7,37 @@ fastprogress.fastprogress.SAVE_PATH = 'log.txt'
 
 path = Config().data_path()/'wikitext-2'
 #path = "/raid/dldata/fastai/wikitext-2"
-data = load_data(path, bs=32, bptt=150)
+
+def istitle(line):
+    return len(re.findall(r'^ = [^=]* = $', line)) != 0
+
+def read_file(filename):
+    articles = []
+    with open(filename, encoding='utf8') as f:
+        lines = f.readlines()
+    current_article = ''
+    for i,line in enumerate(lines):
+        current_article += line
+        if i < len(lines)-2 and lines[i+1] == ' \n' and istitle(lines[i+2]):
+            current_article = current_article.replace('<unk>', UNK)
+            articles.append(current_article)
+            current_article = ''
+    current_article = current_article.replace('<unk>', UNK)
+    articles.append(current_article)
+    return np.array(articles)
+
+train = read_file(path/'train.txt')
+valid = read_file(path/'valid.txt')
+
+all_texts = np.concatenate([valid, train])
+df = pd.DataFrame({'texts':all_texts})
+df.head()
+
+data = (TextList.from_df(df, path, cols='texts')
+                .split_by_idx(range(0,60))
+                .label_for_lm()
+                .databunch(bs=8, bptt=150))
+
 config = tfmerXL_lm_config.copy()
 config['output_p'] = 0.1
 config['embed_p'] = 0.1
