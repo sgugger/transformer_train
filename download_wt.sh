230825ae4c9912bf001dd2cb8ce53ffd27dd3333
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2019-02-25 14:47:19 -0500

    Update

--- download_wt.sh
+++ download_wt.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-pushd ~/.fastai/data/
+ROOT=/raid/dldata/fastai/data
+pushd $ROOT
 wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
 unzip wikitext-103-v1.zip
 wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip
 unzip wikitext-2-v1.zip
 popd
-pushd ~/.fastai/data/wikitext-103/
+pushd $ROOT/wikitext-103/
 mv wiki.train.tokens train.txt
 mv wiki.valid.tokens valid.txt
 mv wiki.test.tokens test.txt
 popd
-pushd ~/.fastai/data/wikitext-2/
+pushd $ROOT/wikitext-2/
 mv wiki.train.tokens train.txt
 mv wiki.valid.tokens valid.txt
 mv wiki.test.tokens test.txt
